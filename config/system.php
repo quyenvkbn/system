bb9fec8d3c61c636e7c64cb698fcff365e9a4fa8
nguyen van quyen <12quyen12@gmail.com>
2020-07-21 10:22:56 +0700

    update v14

--- config/system.php
+++ config/system.php
@@ -20,14 +20,28 @@ return [
 		'meta_description' => array('type' => 'textarea', 'label_or_lang' => 'meta_description'),
 		'meta_image' => array('type' => 'image', 'label_or_lang' => 'meta_image'),
 	],
-
-	/* Select the dropdown table as namepace Model, and the table must use the alanguage column
 	'config' => [
-		'post' => array('type' => 'dropdown', 'table' => 'Modules\Post\Entities\Post', 'first_select' => '--Chọn tin tức--', 'label_or_lang' => 'select_article'),
-	],*/
+		'menu' => array('type' => 'dropdown', 'table' => 'Modules\Menu\Entities\MenuCategory', 'first_select' => '--Chọn main menu--', 'label_or_lang' => 'Menu', 'deleted_at' => false),
+		'slide' => array('type' => 'dropdown', 'table' => 'Modules\Slide\Entities\SlideCategory', 'first_select' => '--Chọn main slide--', 'label_or_lang' => 'Slide', 'deleted_at' => false),
+	],
+	'contact' => [
+		'address' => array('type' => 'text', 'label_or_lang' => 'Địa chỉ'),
+		'hotline' => array('type' => 'text', 'label_or_lang' => 'Hotline'),
+		'fax' => array('type' => 'text', 'label_or_lang' => 'Fax'),
+		'email' => array('type' => 'text', 'label_or_lang' => 'Email'),
+		'website' => array('type' => 'text', 'label_or_lang' => 'Website'),
+		'timework' => array('type' => 'text', 'label_or_lang' => 'Giờ làm việc'),
+		'map' => array('type' => 'textarea', 'label_or_lang' => 'Map'),
+	],
+	'social' => [
+		'facebook' => array('type' => 'text', 'label_or_lang' => 'Facebook'),
+		'youtube' => array('type' => 'text', 'label_or_lang' => 'Youtube'),
+		'twitter' => array('type' => 'text', 'label_or_lang' => 'Twitter'),
+		'instagram' => array('type' => 'text', 'label_or_lang' => 'Instagram'),
+		'google' => array('type' => 'text', 'label_or_lang' => 'Google'),
+	],
 	'script' => [
 		'head' => array('type' => 'textarea', 'label_or_lang' => 'script_head'),
-
 		'body' => array('type' => 'textarea', 'label_or_lang' => 'script_body'),
 	],
 ];
